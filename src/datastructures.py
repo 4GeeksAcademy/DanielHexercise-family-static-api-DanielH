660177b7d93201af991b4c9813b74e992a1bcfe9
Daniel Hurtado Quero <131759030+OconnelDan@users.noreply.github.com>
2023-08-20 17:27:27 +0200

    Update datastructures.py

--- src/datastructures.py
+++ src/datastructures.py
@@ -12,51 +12,43 @@ class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
 
-        self._members = [
-            {
-            "id": self._generateId(),
-            "first_name": "John",
-            "last_name": last_name,
-            "age": 33, 
-            "lucky_Numbers": [7, 13, 22]
-        },
-        {
-            "id": self._generateId(),
-            "first_name": "Jane",
-            "last_name": last_name,
-            "age": 35, 
-            "lucky_Numbers": [10, 14, 3]
-        },
-        {
-            "id": self._generateId(),
-            "first_name": "Jimmy",
-            "last_name": last_name,
-            "age": 5, 
-            "lucky_Numbers": [1]
+        # example list of members
+        self._members = []
 
-        }
-    ]
-
+    # def print_last_name(self):
+    #     print(self.last_name)
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
-        return randint(0, 99999999)
+    
+    # def _generateId(self):
+    #     return randint(0, 99999999)
 
     def add_member(self, member):
         # fill this method and update the return
-        member["id"] = self._generateId() #Agrega un nº aleatorio a id dentro del diccionario
-        member["last_name"] = self.last_name
+        if "id" not in member:
+            member["id"] = randint(0, 99999999)
+
         self._members.append(member)
-        print(self._members)
-        return self._members
+
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        self._members = list(filter(lambda member: member["id"] != id, self._members))
+        # new_family= []
+        # for member in self._members:
+        #     if member['id'] != id:
+        #         new_family.push(member) 
+        # self._members = new_family
+        
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
-
+        member = list(filter(lambda member:member["id"] == id, self._members))
+        
+        if len(member) > 0:
+            return member[0]
+        
+        return None
+        
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
